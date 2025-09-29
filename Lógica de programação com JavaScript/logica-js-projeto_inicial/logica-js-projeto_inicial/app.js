8b42b6c3d6b65ac0983bf4e8212350ad77dfb47a
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-09-29 16:38:46 -0300

    Inclusão do número de tentativa no jogo do número secreto

--- Lógica de programação com JavaScript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Lógica de programação com JavaScript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -2,6 +2,7 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute;
+let tentativas = 1;
 
 // enquanto o chute não for igual ao número secreto
 while (chute != numeroSecreto) {
@@ -9,13 +10,14 @@ while (chute != numeroSecreto) {
     
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`); // usando template string - usar a crase ``
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`); // usando template string - usar a crase ``
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}. Tente novamente!`);
         } else {
             alert(`O número secreto é maior que ${chute}. Tente novamente!`);
         }
+        tentativas++;
     }
 }
 
