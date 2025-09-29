e7bd5ab668bee3407962e169211cd229ca187cb9
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-09-29 14:47:42 -0300

    Adiciona lógica do jogo do número secreto com verificação de chute, feito nas aulas "Exibindo mensagens na tela" e "Comparando variáveis"

--- Lógica de programação com JavaScript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Lógica de programação com JavaScript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,1 +1,9 @@
 alert('Boa vindas ao jogo do número secreto!');
+
+let numeroSecreto = 29;
+let chute = prompt('Digita um número entre 1 e 30:');
+
+if (chute == numeroSecreto) {
+    console.log('Isso ai! Você descobriu o número secreto!');
+
+}
