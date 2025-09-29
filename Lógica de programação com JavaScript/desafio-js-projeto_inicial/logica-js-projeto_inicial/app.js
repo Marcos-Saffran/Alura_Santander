63bbeccfaaefa2711254eddc0e5ffed4a53c9148
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-09-29 16:52:53 -0300

    Alterações para execução de Desafio: hora da prática - de Loop e tentativas

--- Lógica de programação com JavaScript/desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Lógica de programação com JavaScript/desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,33 +1,32 @@
 // Desafios
 
-// 1    Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
-let diaDaSemana = prompt("Qual é o dia da semana?");
-if (diaDaSemana === "Sábado" || diaDaSemana === "Domingo") {
-    alert("Bom fim de semana!");
-} else {
-    alert("Boa semana!");
+// 1    Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+let contador = 1;
+
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
 }
 
-// 2    Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
-let numeroDigitado = prompt("Digite um número:");
-if (numeroDigitado > 0) {
-    alert(`O número ${numeroDigitado} é positivo.`);
-} else {
-    alert(`O número ${numeroDigitado} é negativo.`);
+// 2   Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+let contadorRegressivo = 10;
+while (contadorRegressivo >= 0) {
+    console.log(contadorRegressivo);
+    contadorRegressivo--;
 }
 
-//  3  Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
-let pontuacao = prompt("Digite sua pontuação:");
-if (pontuacao >= 100) {
-    alert("Parabéns, você venceu!");
-} else {
-    alert("Tente novamente para ganhar.");
+// 3    Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+let numeroParaLoop = parseInt(prompt("Digite um número para iniciar a contagem regressiva:"));
+while (numeroParaLoop >= 0) {
+    console.log(numeroParaLoop);
+    numeroParaLoop--;
 }
 
-// 4  Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
-let saldo = 100;
-alert(`Seu saldo da conta é R$ ${saldo}.`);
+// 4    Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+let numeroParaLoopProgressivo = parseInt(prompt("Digite um número para iniciar a contagem progressiva:"));
+let contadorProgressivo = 0;
 
-// 5  Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
-let nome = prompt("Qual é o seu nome?");
-alert(`Bem-vindo(a), ${nome}!`);
+while (contadorProgressivo <= numeroParaLoopProgressivo) {
+    console.log(contadorProgressivo);
+    contadorProgressivo++;
+}
