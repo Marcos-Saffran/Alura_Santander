a06cc180e90db56d671c0cacb448127a17e742cc
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-09-30 16:18:44 -0300

    refactor: ajuste no verificarChute para exibição dos textos na tela, aula Verificando o chute concluída.

--- Lógica explore funções e listas/js-curso-2-aula1/app.js
+++ Lógica explore funções e listas/js-curso-2-aula1/app.js
@@ -12,11 +12,12 @@ exibirTextoNaTela('p', 'Escolha um número entre 0 e 10');
 function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h2', 'Você acertou! O número secreto é ' + numeroSecreto);
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto: ' + numeroSecreto);
     } else if (chute < numeroSecreto) {
-        exibirTextoNaTela('h2', 'O número secreto é maior que ' + chute);
+        exibirTextoNaTela('p', 'O número secreto é maior que ' + chute);
     } else if (chute > numeroSecreto) {
-        exibirTextoNaTela('h2', 'O número secreto é menor que ' + chute);
+        exibirTextoNaTela('p', 'O número secreto é menor que ' + chute);
     }
     console.log(numeroSecreto);
 }
